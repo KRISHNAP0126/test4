01f58fa6ed0a6448972d4937d2fa424969b66610
KRISHNAP0126 <164004877+KRISHNAP0126@users.noreply.github.com>
2024-04-30 13:12:45 +0530

    Add files via upload

---
+++ test 4/script.js
@@ -0,0 +1,20 @@
+var shape = document.getElementById("shape")
+var count = document.getElementById("count")
+
+
+
+// function click(){
+//     var count=math.random()*10
+// }
+
+
+function Red() {
+
+    shape.style = "background-color:red;"
+
+}
+function Blue() {
+
+    shape.style = "background-color:blue;"
+
+}
